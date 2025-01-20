ff50ed87ccdae8d1ab81098804bfa66fdd24c49a
gianght1607 <hagiang160895@gmail.com>
2025-01-20 10:38:34 +0700

    tets

---
+++ Test Suites/testSuiteJenKins.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testSuiteJenKins</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1e0b1ba5-936f-4b68-897d-98f8c0dc6cf8</testSuiteGuid>
+   <testCaseLink>
+      <guid>9dcb3d7f-3c88-478e-951c-337192ef9181</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
